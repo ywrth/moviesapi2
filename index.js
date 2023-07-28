8b59b8a9f52265d44897b069be6a647c14a70f03
ywrth <zuatworknow@gmail.com>
2023-07-28 07:16:43 +0200

    add user update

--- index.js
+++ index.js
@@ -211,17 +211,41 @@ let topMovies = [
 ]
 
 // CREATE user
-app.post('/users', (req, res) => {
-  const newUser = req.body
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 
-  if (newUser.name) {
-    newUser.id = uuid.v4()
-    users.push(newUser)
-    res.status(201).json(newUser)
-  } else {
-    res.status(400).send('users needs names')
-  }
-})
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 // UPDATE user
 app.put('/users/:id', (req, res) => {
