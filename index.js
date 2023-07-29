2d9aa668459e1c1e7940a6a21901a0ff5213ddac
ywrth <zuatworknow@gmail.com>
2023-07-29 11:35:36 +0200

    mongoose update index.js

--- index.js
+++ index.js
@@ -303,9 +303,25 @@ app.put('/users/:Username', async (req, res) => {
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send(‘Error: ’ + err);
+    res.status(500).send('Error: ' + err);
   })
 
+});
+
+// Add a movie to a user's list of favorites
+
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send(Error + err);
+  });
 });
 
 // CREATE movie
@@ -340,20 +356,21 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
   }
 })
 
-// DELETE user
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params
-
-  let user = users.find(user => user.id == id)
-
-  if (user) {
-    users = users.filter(user => user.id != id)
-    res.status(200).send(`user ${id} has been deleted`)
-  } else {
-    res.status(400).send('no such user')
-  }
-})
-
+// Delete a user by username
+app.delete('/users/:Username', async (req, res) => {
+  await Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 
 //READ the list of all movies to the users (CRUD)
