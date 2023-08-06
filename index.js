ed214c2ea1dfbfdfe9af1bf76e7040ddedd49e30
ywrth <zuatworknow@gmail.com>
2023-08-06 07:11:42 +0200

    update

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(cors({
 
 
 //mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true },
+
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Initialize passport and set up passport strategies (local and JWT)
