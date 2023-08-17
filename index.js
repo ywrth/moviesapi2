e88d4ba1c54459001075446f51eefccb3dea6842
ywrth <zuatworknow@gmail.com>
2023-08-17 10:01:05 +0200

    remove auth

--- index.js
+++ index.js
@@ -50,10 +50,7 @@ app.get('/', (req, res) => {
 
 
 //READ THE LIST OF ALL MOVIES
-app.get(
-  '/movies',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
       .then((movies) => res.status(200).json(movies))
       .catch((error) => {
