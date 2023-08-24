7748a85944e6f25faf2dd064352c2bf7b633f271
ywrth <zuatworknow@gmail.com>
2023-08-24 11:42:28 +0200

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ require('./passport');
 
 
 // CORS
-const allowedOrigins = ['http://localhost:1234']; // Replace with your frontend's URL
+const allowedOrigins = ['http://localhost:1234']; 
 app.use(cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
