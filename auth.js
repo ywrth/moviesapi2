c7aff28f5f1427d55591e2e9f4bfb99b2a42e615
ywrth <zuatworknow@gmail.com>
2023-08-25 13:53:59 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -13,25 +13,33 @@ let generateJWTToken = (user) => {
   });
 };
 
-  // Login module
-  module.exports = (router) => {
-    router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user, info) => {
-        console.log(user);
-        if (error || !user) {
-          return res.status(400).json({
-            message: 'Houston, we have a problem!',
-            user,
-          });
-        }
-        req.login(user, { session: false }, (error) => {
-          if (error) {
-            res.send(error);
-          }
-          let token = generateJWTToken(user.toJSON());
-          return res.json({ user, token });
-        });
-      })(req, res);
-    });
-  };
+// Login module
+module.exports = (router) => {
+  // Import the cors module
+  const cors = require('cors');
 
+  // Apply cors middleware for the login route
+  router.post('/login', cors(), (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      console.log(user);
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Houston, we have a problem!',
+          user,
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+
+        // Set appropriate headers in the response
+        res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
+        res.header('Access-Control-Allow-Credentials', true);
+
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
