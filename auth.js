2abeef908db116f356c05bf6a15319e9c63169bf
ywrth <zuatworknow@gmail.com>
2023-08-26 12:47:30 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 require('./passport');
 
-// Function to generate JWT token for authentication
+// Function to generate JWT token
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username,
@@ -13,25 +13,33 @@ let generateJWTToken = (user) => {
   });
 };
 
-  // Login module
-  module.exports = (router) => {
-    router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user, info) => {
-        console.log(user);
-        if (error || !user) {
-          return res.status(400).json({
-            message: 'Houston, we have a problem!',
-            user,
-          });
-        }
-        req.login(user, { session: false }, (error) => {
-          if (error) {
-            res.send(error);
-          }
-          let token = generateJWTToken(user.toJSON());
-          return res.json({ user, token });
-        });
-      })(req, res);
-    });
-  };
+// Login module
+module.exports = (router) => {
+  // Import the cors module
+  const cors = require('cors');
 
+  // Apply cors middleware for the login route
+  router.post('/login', cors(), (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      console.log(user);
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Houston, we have a problem!',
+          user,
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+
+        // Set appropriate headers in the response
+        res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
+        res.header('Access-Control-Allow-Credentials', true);
+
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
