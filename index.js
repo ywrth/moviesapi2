272f1257d4d0287120b86f1e59ad5fcedc6d6cfc
ywrth <zuatworknow@gmail.com>
2023-10-23 15:53:38 +0200

    cors issues

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 //WELCOME
 app.get("/", (req, res) => {
